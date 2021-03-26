9aaf640979e2f2133bc9cca5535d44bae327b3f6
Nemesis076 <68237452+Nemesis076@users.noreply.github.com>
2021-03-26 01:11:12 -0700

    Add files via upload

---
+++ Behavior Trees Scripts/Task.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Timers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Task
+{
+    public abstract void run();
+    public bool succeeded;
+
+    protected int eventId;
+    const string EVENT_NAME_PREFIX = "FinishedTask";
+    public string TaskFinished
+    {
+        get
+        {
+            return EVENT_NAME_PREFIX + eventId;
+        }
+    }
+    public Task()
+    {
+        eventId = EventBus.GetEventID();
+    }
+}
+
+public class IsTrue : Task
+{
+    bool varToTest;
+
+    public IsTrue(bool someBool)
+    {
+        varToTest = someBool;
+
+    }
+
+    public override void run()
+    {
+        succeeded = varToTest;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+
+public class IsFalse : Task
+{
+    bool varToTest;
+
+    public IsFalse(bool someBool)
+    {
+        varToTest = someBool;
+    }
+
+    public override void run()
+    {
+        succeeded = !varToTest;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class OpenDoor : Task
+{
+    Door mDoor;
+
+    public OpenDoor(Door someDoor)
+    {
+        mDoor = someDoor;
+    }
+
+    public override void run()
+    {
+        succeeded = mDoor.Open();
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class BargeDoor : Task
+{
+    Rigidbody mDoor;
+
+    public BargeDoor(Rigidbody someDoor)
+    {
+        mDoor = someDoor;
+    }
+
+    public override void run()
+    {
+        mDoor.AddForce(-10f, 0, 0, ForceMode.VelocityChange);
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class HulkOut : Task
+{
+    GameObject mEntity;
+
+    public HulkOut(GameObject someEntity)
+    {
+        mEntity = someEntity;
+    }
+
+    public override void run()
+    {
+        mEntity.transform.localScale *= 2;
+        mEntity.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class Wait : Task
+{
+    float mTimeToWait;
+
+    public Wait(float time)
+    {
+        mTimeToWait = time;
+    }
+
+    public override void run()
+    {
+        succeeded = true;
+        EventBus.ScheduleTrigger(TaskFinished, mTimeToWait);
+    }
+}
+
+public class MoveKinematicToObject : Task
+{
+    Arriver2 mMover;
+    GameObject mTarget;
+
+    public MoveKinematicToObject(Kinematic mover, GameObject target)
+    {
+        mMover = mover as Arriver2;
+        mTarget = target;
+    }
+
+    public override void run()
+    {
+        mMover.OnArrived += MoverArrived;
+        mMover.myTarget = mTarget;
+    }
+
+    public void MoverArrived()
+    {
+        mMover.OnArrived -= MoverArrived;
+        succeeded = true;
+        EventBus.TriggerEvent(TaskFinished);
+    }
+}
+
+public class Sequence : Task
+{
+    List<Task> children;
+    Task currentTask;
+    int currentTaskIndex = 0;
+
+    public Sequence(List<Task> taskList)
+    {
+        children = taskList;
+    }
+
+    public override void run()
+    {
+        currentTask = children[currentTaskIndex];
+        EventBus.StartListening(currentTask.TaskFinished, OnChildTaskFinished);
+        currentTask.run();
+    }
+
+    void OnChildTaskFinished()
+    {
+        if (currentTask.succeeded)
+        {
+            EventBus.StopListening(currentTask.TaskFinished, OnChildTaskFinished);
+            currentTaskIndex++;
+            if (currentTaskIndex < children.Count)
+            {
+                this.run();
+            }
+            else
+            {
+                succeeded = true;
+                EventBus.TriggerEvent(TaskFinished);
+            }
+
+        }
+        else
+        {
+            succeeded = false;
+            EventBus.TriggerEvent(TaskFinished);
+        }
+    }
+}
+
+public class Selector : Task
+{
+    List<Task> children;
+    Task currentTask;
+    int currentTaskIndex = 0;
+
+    public Selector(List<Task> taskList)
+    {
+        children = taskList;
+    }
+    public override void run()
+    {
+        currentTask = children[currentTaskIndex];
+        EventBus.StartListening(currentTask.TaskFinished, OnChildTaskFinished);
+        currentTask.run();
+    }
+
+    void OnChildTaskFinished()
+    {
+        if (currentTask.succeeded)
+        {
+            succeeded = true;
+            EventBus.TriggerEvent(TaskFinished);
+        }
+        else
+        {
+            EventBus.StopListening(currentTask.TaskFinished, OnChildTaskFinished);
+            currentTaskIndex++;
+            if (currentTaskIndex < children.Count)
+            {
+                this.run();
+            }
+            else
+            {
+                succeeded = false;
+                EventBus.TriggerEvent(TaskFinished);
+            }
+        }
+    }
+}
